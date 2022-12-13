123610d7d9670c038d1071ad66af6b6ea305f995
GabLunaDev <gabriellunacv@gmail.com>
2022-12-12 23:41:36 -0300

    Stating my game development studies

---
+++ Source/LearningCpp/LearningCppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearningCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNINGCPP_API ALearningCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
